846f26c5c46e5fda02cd03721732f9ae8ffb3daa
Joel Watkins <eck087@yahoo.co.uk>
2022-10-19 16:39:17 +0100

    form updated

--- src/index.js
+++ src/index.js
@@ -5,23 +5,49 @@ const dogsList = document.querySelector(".dogs-list");
 const formButton = document.querySelector(".dogs-list__button--add");
 const main = document.querySelector(".main");
 
-console.log(formButton);
+function generateDogs() {
+  data.forEach((dog) => {
+    const li = document.createElement("li");
+    li.classList.add("dogs-list__button");
+    li.innerText = dog.name;
+    dogsList.append(li);
 
-data.forEach((dog) => {
-  const li = document.createElement("li");
-  li.classList.add("dogs-list__button");
-  li.innerText = dog.name;
-  dogsList.append(li);
-
-  li.addEventListener("click", (e) => {
-    const dogHtml = dogCard(dog);
-    main.innerHTML = dogHtml;
+    li.addEventListener("click", (e) => {
+      const dogHtml = dogCard(dog);
+      main.innerHTML = dogHtml;
+    });
   });
-});
+}
+
+generateDogs();
 
 formButton.addEventListener("click", () => {
-  const form = addForm();
-  main.innerHTML = form;
+  const dogForm = addForm();
+  main.innerHTML = dogForm;
+
+  const form = document.querySelector(".form");
+  const dogNameInput = document.querySelector("#name");
+  const dogImageInput = document.querySelector("#image");
+  const dogBioInput = document.querySelector("#bio");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const dogName = dogNameInput.value;
+    const dogImage = dogImageInput.value;
+    const dogBio = dogBioInput.value;
+
+    const dogObject = {
+      name: dogName,
+      image: dogImage,
+      bio: dogBio,
+    };
+
+    data.push(dogObject);
+    dogsList.innerHTML = `
+    <li class="dogs-list__button dogs-list__button--add">+</li>
+  `;
+    generateDogs();
+  });
 });
 
 function dogCard(dog) {
